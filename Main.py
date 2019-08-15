263a69b435145f76eb0df71122bc3e1a99532a5b
MouraYuri <mourayuri339@gmail.com>
2019-08-15 18:16:55 -0300

    o bug é o lobo do programador

--- Main.py
+++ Main.py
@@ -1,46 +1,69 @@
-#
-#
-#
-#
-
 from reg import registers
-
+from dataMemory import dMem as dataMemory
 manipuladorIL = open('input', 'r')
 instructionList = manipuladorIL.readlines()
 
 manipuladorReg = open('registers', 'r')
 regs = manipuladorReg.readlines()
 
-#print(registers)
 
-instructionsVector = ['add', 'addi', 'lw', 'j', 'beq']
+instructionsVector = ['add', 'addi', 'lw', 'j', 'beq'] #instruções aceitas
+instructionIndexRI = [0, 1, 2] #Index das instruções do tipo R e I
+instructionIndexBranchs = [3, 4]
+labels = {}
 
-PC = hex(0)
 lineRead = 0
 
-def execute(instructionLine, registers, instructionIndex):
+def executeRI(instructionLine, registers, instructionIndex, dataMemory):
 
     if (instructionIndex == 1):
         ctrl = registers[instructionLine[2]] + (int(instructionLine[3]))
+    if (instructionIndex == 0):
+        ctrl = registers[instructionLine[2]] + registers[instructionLine[3]]
+    if (instructionIndex == 2):
+        loadedWord = instructionLine[3]
+        loadedWord = loadedWord[1:3]
+        loadedWord = registers[loadedWord]
+        loadedWord = loadedWord + int(instructionLine[2])
+        print('loadedWord = {}'.format(loadedWord))
+        ctrl = dataMemory[loadedWord][0]
 
 
     return ctrl
+def executeBranchs(labels,instructionIndex,instructionLine,lineRead,registers):
+    if(instructionIndex == 3):
+        ctrl = labels[instructionLine[1]]
+    if (instructionIndex == 4):
+        if (registers[instructionLine[1]] == registers[instructionLine[2]]):
+            ctrl = labels[instructionLine[3]]
+        else:
+            ctrl = lineRead
+    return ctrl
 
-
-
-while True:
+while (True):
     print(instructionList[lineRead])
     instructionLine = instructionList[lineRead].split(' ')
-    print('InstructionLine = {}'.format(instructionLine))
-    for x in instructionsVector:
-        if (instructionLine[0] == x):
-            instructionIndex = instructionsVector.index(x)
-            break
-    print('Index of Instruction = {}'.format(instructionIndex))
-    registers[instructionLine[1]] = execute(instructionLine,registers, instructionIndex)
+    if (len(instructionLine) != 1):
+        for x in instructionsVector:
+            if(instructionLine[0] == x):
+                instructionIndex = instructionsVector.index(x)
+                break
+        if (instructionIndex in instructionIndexRI):
+            registers[instructionLine[1]] = executeRI(instructionLine, registers, instructionIndex, dataMemory)
+        if(instructionIndex in instructionIndexBranchs):
+            lineRead = executeBranchs(labels,instructionIndex,instructionLine,lineRead,registers)
 
-    print(registers)
-    break
+    else:
+        keyToBeStored = instructionLine[0]
+        keyToBeStored = keyToBeStored[:3]
+        labels[keyToBeStored] = lineRead
+
+
+
+
+    for z in registers:
+        print('{} -> {}'.format(z, hex(registers[z])))
     lineRead += 1
     if (lineRead >= len(instructionList)):
         break
+    input('Pressione qualquer tecla para pular para próxima iteração')
